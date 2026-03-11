3c6fd2a814576d805c305e138d9ea16e56fbe2a0
StevenAngel <steven.engel1511@gmail.com>
2026-03-11 11:20:11 +0100

    Add NewsSortButton.vue

---
+++ src/components/newsFeed/NewsSortButton.vue
@@ -0,0 +1,21 @@
+<script setup>
+const props = defineProps(['text'])
+</script>
+
+<template>
+    <div class="sortButton">
+        <span>{{ props.text }}</span>
+    </div>
+</template>
+
+<style scoped>
+.sortButton {
+    background-color: var(--bg-surface);
+    padding: 1rem;
+    border-radius: 1rem;
+}
+
+.sortButton:hover {
+    background-color: var(--border-subtle);
+}
+</style>
