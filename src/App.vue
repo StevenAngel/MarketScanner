8528e1e596b4b0fbc717391de845480f5a6281e8
StevenAngel <steven.engel1511@gmail.com>
2026-03-11 10:28:08 +0100

    Changed global style from app.vue to style.css

--- src/App.vue
+++ src/App.vue
@@ -9,20 +9,3 @@ import Navbar from './components/layout/Navbar.vue'
 </template>
 
 <style scoped></style>
-
-<style>
-h1 {
-  font-size: 2rem;
-}
-
-table {
-  border-collapse: separate;
-  border-spacing: 0;
-  text-align: center;
-}
-
-td,
-th {
-  padding: 1rem;
-}
-</style>
