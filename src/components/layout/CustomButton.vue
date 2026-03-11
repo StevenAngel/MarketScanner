e0cdf9d50feeefebce3aad6af69a695bccff2b2b
StevenAngel <steven.engel1511@gmail.com>
2026-03-11 16:05:56 +0100

    Add custom button component

---
+++ src/components/layout/CustomButton.vue
@@ -0,0 +1,24 @@
+<script setup>
+const props = defineProps(['text'])
+</script>
+
+<template>
+    <div class="button">
+        <span>{{ props.text }}</span>
+    </div>
+</template>
+
+<style scoped>
+.button {
+    background-color: var(--bg-surface);
+    border: 1px solid var(--border-subtle);
+    padding: .5rem 1rem;
+    cursor: pointer;
+    width: fit-content;
+    border-radius: .5rem;
+}
+
+.button:hover {
+    background-color: var(--border-subtle);
+}
+</style>
