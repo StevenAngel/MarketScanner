316986029fd0b2c3d7b1429ab6488355111ef7b0
StevenAngel <steven.engel1511@gmail.com>
2026-03-11 15:49:16 +0100

    Fix slice error

--- src/components/newsFeed/NewsFeedSidebar.vue
+++ src/components/newsFeed/NewsFeedSidebar.vue
@@ -3,7 +3,8 @@ import { onMounted, ref } from 'vue';
 import NewsItem from './NewsItem.vue';
 
 const feed = ref([]);
-// Backend needed
+
+// Load rss news feed from backend
 const loadFeed = async () => {
     feed.value = await fetch('http://localhost:3000/rss').then(res => res.json()).catch(err => console.error(err))
 }
@@ -14,8 +15,8 @@ onMounted(loadFeed)
 <template>
     <div class="newsSidebar">
         <h2>News Feed</h2>
-        <NewsItem v-for="(item, index) in feed.slice(0, 20)" :key="index" :imgSrc="item.imageUrl" :text="item.title"
-            :link="item.link" />
+        <NewsItem v-for="(item, index) in feed.news?.slice(0, 20)" :key="index" :imgSrc="item.imageUrl"
+            :text="item.title" :link="item.link" />
     </div>
 </template>
 
