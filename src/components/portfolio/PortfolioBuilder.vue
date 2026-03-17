6bf9a97e050360a7720f932d22757f364a1c0bba
StevenAngel <steven.engel1511@gmail.com>
2026-03-17 11:51:55 +0100

    Add simple linechart with data

--- src/components/portfolio/PortfolioBuilder.vue
+++ src/components/portfolio/PortfolioBuilder.vue
@@ -13,7 +13,19 @@ import { appState } from '../stores/crypto.js'
 import LineChart from '../charts/LineChart.vue';
 const selectableCoins = ref([])
 const selectedCoins = ref([])
+let linechartData = {}
+const linechartDataX = ref([])
+const linechartDataY = ref([])
 const coinHistory = {}
+const loadingOptions = {
+    text: 'Loading...',
+    color: '#6366f1',
+    textColor: '#fff',
+    fontSize: 24,
+    fontWeight: 600,
+    maskColor: 'rgba(0, 0, 0, 0.8)',
+    zlevel: 0
+};
 
 // Fetch coin history from binance (backend)
 const fetchCoin = async (coin, isChecked) => {
@@ -22,7 +34,10 @@ const fetchCoin = async (coin, isChecked) => {
         if (!coinHistory[coin]) {
             const history = await fetch("http://localhost:3000/coinHistory", {
                 method: "POST",
-                body: JSON.stringify({ coin })
+                headers: {
+                    "Content-Type": "application/json" // Sag dem Server: "Hier kommt JSON!"
+                },
+                body: JSON.stringify({ coin: coin })
             }).then(res => res.json()).catch(err => console.error(err))
             coinHistory[coin] = history
         }
@@ -30,6 +45,25 @@ const fetchCoin = async (coin, isChecked) => {
         const index = selectedCoins.value.indexOf(coin)
         if (index > -1) selectedCoins.value.splice(index, 1)
     }
+
+    updateLineChart()
+}
+
+const updateLineChart = () => {
+    linechartData = {}
+    selectedCoins.value.forEach(coin => {
+        coinHistory[coin].forEach(value => {
+            if (!linechartData[value.time]) {
+                linechartData[value.time] = Number(value.price)
+            } else {
+                linechartData[value.time] += Number(value.price)
+            }
+        })
+    })
+
+
+    linechartDataX.value = Object.entries(linechartData).map(value => value[0])
+    linechartDataY.value = Object.entries(linechartData).map(value => value[1])
 }
 
 // Load markets if not already done
@@ -47,9 +81,13 @@ onMounted(loadMarkets)
 </script>
 
 <template>
-    <!-- v-model for bidirectional tunnel from parent to child and child to parent -->
     <CryptoSelector :selectableCoins="selectableCoins" @coinSelected="fetchCoin" />
-    <LineChart />
+    <LineChart class="chart" :lineChartDataX="linechartDataX" :lineChartDataY="linechartDataY" :loading="false"
+        :loading-options="loadingOptions" />
 </template>
 
-<style scoped></style>
+<style scoped>
+.chart {
+    height: 400px;
+}
+</style>
